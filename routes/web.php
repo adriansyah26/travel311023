3524eb5b742114aed310b086db8ebc5bbbba35e7
adriansyah26 <adriansyah2653@gmail.com>
2023-06-19 14:29:25 +0700

    login dan home

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return redirect()->route('home');
+})->name('welcome');
+
+Route::view('/home', 'home')->name('home');
+Route::get('/register', [\App\Http\Controllers\RegisterController::class, 'create'])->name('register')->middleware('guest');
+Route::post('/register', [\App\Http\Controllers\RegisterController::class, 'store']);
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'authenticate']);
+Route::post('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect('/');
+})->name('logout')->middleware('auth');
+Route::view('/home', 'home')->name('home')->middleware('auth');
+
